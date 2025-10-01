a78b50734e693c97fc16182d98048f2f3a50b47d
Henrique <hebaja@yahoo.com.br>
2025-10-01 11:49:09 -0300

    refactor: 💡 Module00
    Refactoring ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -1,29 +1,28 @@
 #include <iostream>
 #include <cctype>
+#include <string>
 
 using namespace std;
 
-void	put_str(char *str)
-{
-	int	i;
-
-	i = -1;
-	while (str[++i])
-		cout << (char)toupper(str[i]);
-}
-
 int	main(int argc, char **argv)
 {
-	int	i;
+	std::string		str;
+	int				i;
 
-	i = 0;
 	if (argc > 1)
-		while (argv[++i])
-			put_str(argv[i]);
+	{
+		argv++;
+		while (*argv)
+		{
+			i = -1;
+			str = *argv;
+			while (str[++i])
+				std::cout << static_cast<char>(toupper(str[i]));
+			argv++;
+		}
+	}
 	else
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-
-	cout << endl;
-
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << std::endl;
 	return (0);
 }
