de122dbceed560b8b0b206e27640098637468028
Henrique <hebaja@yahoo.com.br>
2025-10-06 19:36:02 -0300

    feat: 🎸 ex01
    Working on ADD command

--- 00/ex01/phonebook.cpp
+++ 00/ex01/phonebook.cpp
@@ -11,14 +11,16 @@ class	Contact
 	std::string	last_name;
 	std::string	nickname;
 	std::string	phone_number;
+	std::string	darkest_secret;
 	public:
 		Contact(){};
-		Contact(int index, std::string first_name, std::string last_name, std::string nickname, std::string phone_number) {
+		Contact(int index, std::string first_name, std::string last_name, std::string nickname, std::string phone_number, std::string darkest_secret) {
 			this->index = index;
 			this->first_name = first_name;
 			this->last_name = last_name;
 			this->nickname = nickname;
 			this->phone_number = phone_number;
+			this->darkest_secret = darkest_secret;
 		};
 		int	getIndex()
 		{
@@ -39,6 +41,10 @@ class	Contact
 		std::string getPhoneNumber()
 		{
 			return this->phone_number;
+		};
+		std::string getDarkestSecret()
+		{
+			return this->darkest_secret;
 		};
 };
 
@@ -86,13 +92,44 @@ class	Phonebook
 
 int	main()
 {
-	Phonebook phonebook(8);
+	std::string	command;
+	Phonebook	phonebook(8);
+
+	std::cout << "Input a command: ADD | SEARCH | EXIT" << std::endl;
+	std::cin >> command;
+	std::cout << command << endl;
+	if (command.compare("ADD") == 0)
+	{
+		// Contact		*contact;
+		std::string	first_name;
+		std::string	last_name;
+		std::string	nickname;
+		std::string	phone_number;
+		std::string	darkest_secret;
+
+		std::cout << "Input contact first name:" << std::endl;
+		std::cin >> first_name;
+		std::cout << "Input contact last name:" << std::endl;
+		std::cin >> last_name;
+		std::cout << "Input contact nickname:" << std::endl;
+		std::cin >> nickname;
+		std::cout << "Input contact phone number:" << std::endl;
+		std::cin >> phone_number;
+		std::cout << "Input contact darkest secret:" << std::endl;
+		std::cin >> darkest_secret;
+		Contact contact_a(0, first_name, last_name, nickname, phone_number, darkest_secret);
+		
+		phonebook.addContact(contact_a, contact_a.getIndex());
+	}
+
+	/*
 	Contact contact_a(0, "Habiloaldo", "Ambr", "aldo", "555-5555");
 	Contact contact_b(1, "Ale", "Peroba", "xandy", "777-7777");
 	Contact contact_c(2, "Alexsandrino", "Peroba", "xandy", "777-7777");
 	phonebook.addContact(contact_a, contact_a.getIndex());
 	phonebook.addContact(contact_b, contact_b.getIndex());
 	phonebook.addContact(contact_c, contact_c.getIndex());
+	*/
 	phonebook.print_all_contacts();
 	return (0);
 }
